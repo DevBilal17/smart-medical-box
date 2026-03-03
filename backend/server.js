b28f7588c7322e171e6540e9eb0a22bf490d7629
DevBilal17 <muhammadbilalmb03122@gmail.com>
2026-03-03 15:00:42 +0500

    Checking

--- backend/server.js
+++ backend/server.js
@@ -84,6 +84,7 @@ app.use('*', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
+  console.log("hello server")
   console.log(`✅ Server running on port ${PORT}`);
   console.log(`✅ WebSocket running on port ${process.env.SOCKET_PORT || 5001}`);
 });
